6eeb98204b81499bf8c93e00a6226115667ed368
smj <shiminjia2019@gmail.com>
2021-02-25 22:57:48 +0900

    20210225

---
+++ pages/posts/[id].js
@@ -0,0 +1,36 @@
+import Layout from '../../components/layout'
+import Date from '../../components/date'
+import { getAllPostIds, getPostData } from '../../lib/posts'
+import Head from 'next/head'
+
+export default function Post({ postData }) {
+    return (
+        <Layout>
+            <Head>
+                <title>{postData.title}</title>
+            </Head>
+            {postData.title}
+            <br />
+            {postData.id}
+            <br />
+            <Date dateString={postData.date} />
+        </Layout>
+    )
+}
+
+export async function getStaticPaths() {
+    const paths = getAllPostIds()
+    return {
+        paths,
+        fallback: false
+    }
+}
+
+export async function getStaticProps({ params }) {
+    const postData = getPostData(params.id)
+    return {
+        props: {
+            postData
+        }
+    }
+}
